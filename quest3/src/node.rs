a4d81c2e1ff8c517c75cee239aa9f901ae7d91f7
Uncle Scientist <unclescientist@gmail.com>
2026-03-18 07:54:35 -0400

    Quest 3, part 2

---
+++ quest3/src/node.rs
@@ -0,0 +1,111 @@
+use std::{convert::Infallible, fmt::Display, str::FromStr};
+
+use crate::socket::Socket;
+
+#[derive(Debug, Default, Clone)]
+pub struct Node {
+    id: usize,
+    plug: Socket,
+
+    left_socket: Socket,
+    left: Option<Box<Node>>,
+
+    right_socket: Socket,
+    right: Option<Box<Node>>,
+}
+
+impl Node {
+    pub fn strong_insert(&mut self, new_node: Node) -> bool {
+        if let Some(ref mut left) = self.left {
+            // TODO: don't clone
+            if left.strong_insert(new_node.clone()) {
+                return true;
+            }
+        } else if new_node.plug == self.left_socket {
+            self.left = Some(Box::new(new_node));
+            return true;
+        }
+
+        if let Some(ref mut right) = self.right {
+            if right.strong_insert(new_node) {
+                return true;
+            }
+        } else if new_node.plug == self.right_socket {
+            self.right = Some(Box::new(new_node));
+            return true;
+        }
+        false
+    }
+
+    pub fn weak_insert(&mut self, new_node: Node) -> bool {
+        if let Some(ref mut left) = self.left {
+            // TODO: don't clone
+            if left.weak_insert(new_node.clone()) {
+                return true;
+            }
+        } else if new_node.plug.matches(&self.left_socket) {
+            self.left = Some(Box::new(new_node));
+            return true;
+        }
+
+        if let Some(ref mut right) = self.right {
+            if right.weak_insert(new_node) {
+                return true;
+            }
+        } else if new_node.plug.matches(&self.right_socket) {
+            self.right = Some(Box::new(new_node));
+            return true;
+        }
+        false
+    }
+
+    pub fn order(&self, ids: &mut Vec<usize>) {
+        if let Some(left) = &self.left {
+            left.order(ids);
+        }
+        ids.push(self.id);
+        if let Some(right) = &self.right {
+            right.order(ids);
+        }
+    }
+
+    fn display(&self, indent: usize, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(left) = &self.left {
+            left.display(indent + 1, f)?;
+        }
+        writeln!(
+            f,
+            "{:indent$} {} plug={} left={} right={}",
+            "", self.id, self.plug, self.left_socket, self.right_socket
+        )?;
+        if let Some(right) = &self.right {
+            right.display(indent + 1, f)?;
+        }
+        Ok(())
+    }
+}
+
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.display(0, f)
+    }
+}
+
+impl FromStr for Node {
+    type Err = Infallible;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let words = line.split(", ").collect::<Vec<_>>();
+        let (_, id) = words[0].split_once('=').unwrap();
+        let (_, plug) = words[1].split_once('=').unwrap();
+        let (_, left_socket) = words[2].split_once('=').unwrap();
+        let (_, right_socket) = words[3].split_once('=').unwrap();
+        Ok(Self {
+            id: id.parse().unwrap(),
+            plug: plug.parse().unwrap(),
+            left_socket: left_socket.parse().unwrap(),
+            right_socket: right_socket.parse().unwrap(),
+            ..Self::default()
+        })
+    }
+}
---
+++ quest3/src/socket.rs
@@ -0,0 +1,131 @@
+use std::{convert::Infallible, fmt::Display, str::FromStr};
+
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
+pub struct Socket {
+    color: Color,
+    shape: Shape,
+}
+
+impl FromStr for Socket {
+    type Err = Infallible;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let (color, shape) = line.split_once(' ').unwrap();
+        Ok(Self {
+            color: color.parse().unwrap(),
+            shape: shape.parse().unwrap(),
+        })
+    }
+}
+
+impl Socket {
+    pub fn matches(&self, other: &Self) -> bool {
+        self.color == other.color || self.shape == other.shape
+    }
+}
+
+impl Display for Socket {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{} {}]", self.color, self.shape)
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
+enum Shape {
+    #[default]
+    Triangle,
+    Square,
+    Diamond,
+    Pentagon,
+    Star,
+    Hexagon,
+    Octagon,
+    Circle,
+}
+
+impl FromStr for Shape {
+    type Err = Infallible;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        Ok(match line {
+            "TRIANGLE" => Self::Triangle,
+            "SQUARE" => Self::Square,
+            "DIAMOND" => Self::Diamond,
+            "PENTAGON" => Self::Pentagon,
+            "STAR" => Self::Star,
+            "HEXAGON" => Self::Hexagon,
+            "OCTAGON" => Self::Octagon,
+            "CIRCLE" => Self::Circle,
+            _ => panic!("Unknown shape {line}"),
+        })
+    }
+}
+
+impl Display for Shape {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Triangle => "TRIANGLE",
+                Self::Square => "SQUARE",
+                Self::Diamond => "DIAMOND",
+                Self::Pentagon => "PENTAGON",
+                Self::Star => "STAR",
+                Self::Hexagon => "HEXAGON",
+                Self::Octagon => "OCTAGON",
+                Self::Circle => "CIRCLE",
+            }
+        )
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
+enum Color {
+    #[default]
+    Blue,
+    White,
+    Green,
+    Red,
+    Black,
+    Cyan,
+    Magenta,
+    Yellow,
+}
+
+impl FromStr for Color {
+    type Err = Infallible;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        Ok(match line {
+            "BLUE" => Self::Blue,
+            "WHITE" => Self::White,
+            "GREEN" => Self::Green,
+            "RED" => Self::Red,
+            "BLACK" => Self::Black,
+            "CYAN" => Self::Cyan,
+            "MAGENTA" => Self::Magenta,
+            "YELLOW" => Self::Yellow,
+            _ => panic!("Unknown color {line}"),
+        })
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Blue => "BLUE",
+                Self::White => "WHITE",
+                Self::Green => "GREEN",
+                Self::Red => "RED",
+                Self::Black => "BLACK",
+                Self::Cyan => "CYAN",
+                Self::Magenta => "MAGENTA",
+                Self::Yellow => "YELLOW",
+            }
+        )
+    }
+}
